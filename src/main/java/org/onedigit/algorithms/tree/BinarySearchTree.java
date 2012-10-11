9c4f4fb74ee208ce881fd991a8b297e21ca859e2
RIZAA <RIZAA@C0155.daiwaeurope.local>
2012-10-11 16:51:59 +0100

    Re-organise tests

--- src/main/java/org/onedigit/algorithms/tree/BinarySearchTree.java
+++ src/main/java/org/onedigit/algorithms/tree/BinarySearchTree.java
@@ -27,18 +27,19 @@ public class BinarySearchTree<T extends Comparable<? super T>>
     
     // ------------------------------------------------------------------------    
     
-    public void insert(Node<T> node)
+    public void insert(T key)
     {
         Node<T> y = null;
         Node<T> x = root;
         while (x != null) {
             y = x;
-            if (node.key.compareTo(x.key) < 0) {
+            if (key.compareTo(x.key) < 0) {
                 x = x.left;
             } else {
                 x = x.right;
             }
         }
+        Node<T> node = new Node<T>(key);
         node.pred = y;
         if (y == null) {
             root = node; // Tree was empty
@@ -167,44 +168,5 @@ public class BinarySearchTree<T extends Comparable<? super T>>
         }
     }
     
-    @Override
-    public String toString()
-    {
-        StringBuilder sb = new StringBuilder();
-        return sb.toString();
-    }
-    
     // ------------------------------------------------------------------------    
-    
-    public static void main(String[] args)
-    {
-        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
-        Node<Integer> six = new Node<Integer>(6); 
-        Node<Integer> five_1 = new Node<Integer>(5); 
-        Node<Integer> eleven = new Node<Integer>(11); 
-        Node<Integer> twelve = new Node<Integer>(12); 
-        Node<Integer> seven = new Node<Integer>(7); 
-        Node<Integer> eight = new Node<Integer>(8); 
-        Node<Integer> nine = new Node<Integer>(9); 
-        Node<Integer> two = new Node<Integer>(2); 
-        Node<Integer> five_2 = new Node<Integer>(5);
-
-
-        bst.insert(six);
-        bst.insert(five_1);
-        bst.insert(eleven);
-        bst.insert(twelve);
-        bst.insert(nine);
-        bst.insert(two);
-        bst.insert(five_2);
-
-        System.out.print("InOrder: " + bst.inOrderTreeWalk());
-        System.out.println();
-        
-        System.out.print("PreOrder: " + bst.preOrderTreeWalk());
-        System.out.println();
-        
-        System.out.print("PostOrder: " + bst.postOrderTreeWalk());
-        System.out.println();
-    }
 }
--- src/test/java/org/onedigit/algorithms/tree/BinarySearchTreeTest.java
+++ src/test/java/org/onedigit/algorithms/tree/BinarySearchTreeTest.java
@@ -10,24 +10,7 @@ public class BinarySearchTreeTest
     public void test()
     {
         BinarySearchTree<Integer> bst = new BinarySearchTree<>();
-        Node<Integer> six = new Node<Integer>(6); 
-        Node<Integer> four = new Node<Integer>(4); 
-        Node<Integer> eleven = new Node<Integer>(11); 
-        Node<Integer> twelve = new Node<Integer>(12); 
-        Node<Integer> seven = new Node<Integer>(7); 
-        Node<Integer> eight = new Node<Integer>(8); 
-        Node<Integer> nine = new Node<Integer>(9); 
-        Node<Integer> two = new Node<Integer>(2); 
-        Node<Integer> five = new Node<Integer>(5);
-
-
-        bst.insert(six);
-        bst.insert(four);
-        bst.insert(eleven);
-        bst.insert(twelve);
-        bst.insert(nine);
-        // bst.insert(two);
-        bst.insert(five);
+        makeTree(bst, 11, 2, 14, 1, 7, 15, 5, 8, 4);
 
         System.out.print("InOrder: " + bst.inOrderTreeWalk());
         System.out.println();
@@ -37,11 +20,13 @@ public class BinarySearchTreeTest
         
         System.out.print("PostOrder: " + bst.postOrderTreeWalk());
         System.out.println();
-        
-        System.out.println("-------------------------------------");
-        
-        bst.remove(four);
-        System.out.print("InOrder: " + bst.inOrderTreeWalk());
-        System.out.println();
+    }
+    
+    public <T extends Comparable<? super T>> void makeTree(BinarySearchTree<T> tree,
+    		T... args)
+    {
+    	for (T t : args) {
+    		tree.insert(t);
+    	}
     }
 }
