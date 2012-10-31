5719f26ef40eef367d63d28bea6f2b5c3cf4ad52
RIZAA <RIZAA@C0155.daiwaeurope.local>
2012-10-31 14:23:55 +0000

    newton

--- src/main/java/org/onedigit/algorithms/maths/NewtonRaphson.java
+++ src/main/java/org/onedigit/algorithms/maths/NewtonRaphson.java
@@ -1,5 +1,7 @@
 package org.onedigit.algorithms.maths;
 
+import org.onedigit.algorithms.misc.TimerUtility;
+
 /**
  * Newton's method for calculating the square root.
  * 
@@ -34,16 +36,34 @@ public class NewtonRaphson
 	public double eval(double x0, double a)
 	{
 	    double x1 = 0.0;
-		int iteration = 0;
+		// int iteration = 0;
 		double f0 = f(x0, a);
-		while (Math.abs(f0) > TOLERANCE && iteration < MAX_ITERATIONS) {
+		while (Math.abs(f0) > TOLERANCE) { // && iteration < MAX_ITERATIONS) {
             x1 = x0 - f0 / fdash(x0);
 			x0 = x1;
 			f0 = f(x0, a);
-			iteration++;
+			// iteration++;
 		}
-		System.out.println("Root = " + x1 + " iterations = " + iteration);
+		// System.out.println("Root = " + x1 + " iterations = " + iteration);
 		return x1;
+	}
+	
+	public void benchmark()
+	{
+		int num = 10_000_000;
+		TimerUtility.start();
+		double result;
+		for (int i = 0; i < num; i++) {
+			result = eval(2.0, 1.0);
+		}
+		double elapsed = TimerUtility.end();
+		System.out.println("NR elpased = " + elapsed + " ms");
+		TimerUtility.start();
+		for (int i = 0; i < num; i++) {
+			result = Math.sqrt(2.0);
+		}
+		elapsed = TimerUtility.end();
+		System.out.println("Math.sqrt elpased = " + elapsed + " ms");
 	}
 	
 	public static void main(String... args)
@@ -52,5 +72,6 @@ public class NewtonRaphson
 		double a = 2.0;
 		double result = nr.eval(1.0, a);
 		System.out.println(Math.sqrt(a) - result);
+		nr.benchmark();
 	}
 }
